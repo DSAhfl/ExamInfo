f28b91e17486323e42138fc313d757b5291b4128
minjxu <xuminjieleon@163.com>
2018-03-21 16:45:20 +0800

    Initialize the program , and only include files which are not relevant to my ExamInfo to test the environment.

---
+++ src/com/test/springmvc/jdbctest/Admin.java
@@ -0,0 +1,23 @@
+package com.test.springmvc.jdbctest;
+
+public class Admin {
+	private int id;
+
+	public Admin() {
+
+	}
+
+	public Admin(int id) {
+		super();
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
---
+++ src/com/test/springmvc/jdbctest/adminJDBCTemplate.java
@@ -0,0 +1,29 @@
+package com.test.springmvc.jdbctest;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class adminJDBCTemplate implements AdminDAO {
+
+	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplateObject;
+
+	@Override
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+	}
+
+	@Override
+	public List<Admin> listAdmins() {
+	      String SQL = "select * from admin";
+	      List <Admin> admins = jdbcTemplateObject.query(SQL, 
+	                                new AdminMapper());
+	      return admins;
+	}
+
+
+}
